a2f011e2eb440bb7bdbef5b5cce63b0c04dc923d
Faeza Zafar <faezazafar@gmail.com>
2026-08-15 15:54:48 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> arr = new HashSet<>();
+        for(int num : nums)
+        {
+            if(arr.contains(num))
+                return true;
+            arr.add(num);
+        }
+        return false;
+    }
+}
